dddedddcdb5cdfe28f4c30b98287114d3ee83a09
louremipsum <myself.vinayak2@gmail.com>
2022-10-11 18:46:21 +0530

    bug: key in map

--- src/components/Navbar/Navlink.js
+++ src/components/Navbar/Navlink.js
@@ -2,15 +2,33 @@ import React from "react";
 import { Button } from "@mantine/core";
 import { Link } from "react-router-dom";
 
-function navlink() {
+function Navlink() {
+  const links = [
+    {
+      title: "About",
+      rel: "about/",
+    },
+    {
+      title: "Contact",
+      rel: "contact/",
+    },
+    {
+      title: "Lab",
+      rel: "experiments/",
+    },
+  ];
+
+  const UID = () => Math.random().toString(36).substring(2, 9);
+
   return (
     <Button.Group>
-      <Button component={Link} to="about/" variant="subtle">
-        About
-      </Button>
-      <Button variant="subtle">Contact</Button>
+      {links.map((item) => (
+        <Button component={Link} to={item.rel} key={UID()} variant="subtle">
+          {item.title}
+        </Button>
+      ))}
     </Button.Group>
   );
 }
 
-export default navlink;
+export default Navlink;
